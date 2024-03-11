233d480382bbf60c1499494de3e3e552cded8b7f
Kasten33 <hurlrykaden@gmail.com>
2024-03-11 17:42:34 -0600

    env file wont read on netlify

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend("re_dB6Mmfsx_7wiihAiV56aF2KNvkGfWpq3s");
 const fromEmail = process.env.FROM_EMAIL;
 
 export async function POST(req, res) {
