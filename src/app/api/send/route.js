bb27a2fba796b220203a10e59fc1381bbc2992d5
Kasten33 <hurlrykaden@gmail.com>
2024-03-11 16:38:47 -0600

    am now working on specializing it

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -7,22 +7,17 @@ const fromEmail = process.env.FROM_EMAIL;
 export async function POST(req, res) {
   const { email, subject, message } = await req.json();
   console.log(email, subject, message);
-  try {
-    const data = await resend.emails.send({
-      from: fromEmail,
-      to: [fromEmail, email],
-      subject: subject,
-      react: (
-        <>
-          <h1>{subject}</h1>
-          <p>Thank you for contacting us!</p>
-          <p>New message submitted:</p>
-          <p>{message}</p>
-        </>
-      ),
-    });
-    return NextResponse.json(data);
-  } catch (error) {
-    return NextResponse.json({ error });
-  }
+  await resend.sendEmail({
+    from: "YOUR NAME <onboarding@resend.dev>",
+    to: [fromEmail, email],
+    subject: subject,
+    react: (
+      <>
+        <p>Thanks for sending an email, i hope to get back to you soon</p>
+      </>
+    ),
+  });
+  return NextResponse.json({
+    status: "ok",
+  });
 }
